a51d454336a2741f68582493c4f06904391b70e6
amargajula123 <amargajula12@gmai.com>
2025-02-13 11:00:30 +0530

    13th commit

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.4"
 AUTHOR="Amar"
 DESCRIPTION="This is the 2025 ML project"
-PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
     """Description: This function is goign to return list of 
     requiremets mentioned in requirements.txt file
     
-    returns This function is going to return a list ,
+    returns This function is going to return a list 
     which contain name of libraries mentioned in the requirements.txt """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines()#.remove("-e .")
     
 
 
@@ -25,13 +24,8 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESCRIPTION,
-packages=PACKAGES,
 
+packages=find_packages(),
 install_requires=get_requirements_list()
-
-
-
 )
-
-#if __name__=="__main__":
-  #  print(get_requirements_list)
+# find_packages() will search all the packages that you are created and its going to return that names
