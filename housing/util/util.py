a3ef6c5dcb8f590f6b5e8301ae75abc1ea3d9cca
amargajula123 <amargajula12@gmai.com>
2025-02-14 13:37:18 +0530

    new cofigarations has made

---
+++ housing/util/util.py
@@ -0,0 +1,15 @@
+import yaml
+from housing.exception import HosingException
+import os,sys
+
+def read_yaml_file(file_path:str)->dict:
+    """
+    Read a YAML file and returns the contents as a dictionary
+    file_path:str
+    """
+
+    try:
+        with open(file_path,"rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HosingException(e,sys) from e
